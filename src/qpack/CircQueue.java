1aadf2be7d09a1887617964f7d87b12a2b5aac17
40katty04 <40katty04@gmail.com>
2018-04-18 21:02:49 +0300

    added

---
+++ src/qpack/CircQueue.java
@@ -0,0 +1,33 @@
+package qpack;
+
+public class CircQueue implements ICharQ {
+    private char q[];
+    private int putloc, getloc;
+
+    public CircQueue(int size){
+        q = new char[size+1];
+        putloc = getloc = 0;
+    }
+
+    public void put (char ch){
+        if (putloc+1==getloc|((putloc==q.length-1)&(getloc==0))){
+            System.out.println(" - Очередь заполнена!");
+            return;
+        }
+
+        putloc++;
+        if (putloc==q.length) putloc = 0;
+        q [putloc]=ch;
+    }
+
+    public char get (){
+        if (getloc==putloc){
+            System.out.println(" - Очередь пуста!");
+            return (char) 0;
+        }
+
+        getloc++;
+        if (getloc==q.length)getloc=0;
+        return q [getloc];
+    }
+}
